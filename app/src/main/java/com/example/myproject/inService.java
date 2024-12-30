831589effc1e214eea3a4233924e52e5cad29f13
diajameel <dianawawreh352000@gmail.com>
2024-12-30 14:06:19 +0200

    adding activities
    adding activities without connecting

---
+++ app/src/main/java/com/example/myproject/inService.java
@@ -0,0 +1,31 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class inService extends AppCompatActivity {
+    RecyclerView inServiceView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_in_service);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        InServiceRecyclerAdapter adapter=new InServiceRecyclerAdapter(this);
+        inServiceView=findViewById(R.id.inServiceView);
+        inServiceView.setAdapter(adapter);
+        inServiceView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
