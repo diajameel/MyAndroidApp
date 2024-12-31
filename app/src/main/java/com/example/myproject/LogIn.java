d8c56555c0f7bb8f6bb2f6a1d2817e2f4955ebbd
diajameel <dianawawreh352000@gmail.com>
2024-12-31 13:27:07 +0200

    dia shared preference
    login shared preference adding

--- app/src/main/java/com/example/myproject/LogIn.java
+++ app/src/main/java/com/example/myproject/LogIn.java
@@ -1,8 +1,11 @@
 package com.example.myproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
@@ -12,9 +15,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LogIn extends AppCompatActivity {
+    EditText editTextUsername;
+    EditText editTextPassword;
+    CheckBox checkRemember;
     Button  btnLogin;
     Button  btnSignin;
     RadioButton radCust,radGarage;
+    SharedPreferences sharedPreference;
+    public static final String SHARED_PREFERENCE_NAME="sharedPreference";
+    public static final String USER_NAME_KEY="userName";
+    public static final String PASSWORD_KEY="password";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +36,34 @@ public class LogIn extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
               });
-        radCust=new RadioButton(this);
-        radGarage=new RadioButton(this);
-
+        sharedPreference=getSharedPreferences(SHARED_PREFERENCE_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreference.edit();
+        editTextPassword=findViewById(R.id.editTextPassword);
+        editTextUsername=findViewById(R.id.editTextUsername);
+        checkRemember=findViewById(R.id.checkRemember);
+        radCust=findViewById(R.id.radCust);
+        radGarage=findViewById(R.id.radGarage);
         btnLogin=findViewById( R.id.btnLogin);
+        String userName=sharedPreference.getString(USER_NAME_KEY,null);
+        String password=sharedPreference.getString(PASSWORD_KEY,null);
+        if(userName!=null&&password!=null){
+            editTextUsername.setText(userName);
+            editTextPassword.setText(password);
+        }
         btnLogin.setOnClickListener(e->{
-if(radCust.isChecked()) {
-    Intent intent = new Intent(LogIn.this, signUp.class);
-    startActivity(intent);
-} else if (radGarage.isChecked()) {
-    Intent intent = new Intent(LogIn.this, GarageSignUp.class);
-    startActivity(intent);
-}
-
-});
+            if(checkRemember.isChecked()){
+                editor.putString(USER_NAME_KEY,editTextUsername.getText().toString());
+                editor.putString(PASSWORD_KEY,editTextPassword.getText().toString());
+                editor.apply();
+            }
+            if(radCust.isChecked()) {
+                Intent intent = new Intent(LogIn.this, signUp.class);
+                startActivity(intent);
+            } else if (radGarage.isChecked()) {
+                Intent intent = new Intent(LogIn.this, GarageSignUp.class);
+                startActivity(intent);
+            }
+        });
         btnSignin=findViewById( R.id.btnSignin);
         btnSignin.setOnClickListener(e->{
             if(radCust.isChecked()) {
